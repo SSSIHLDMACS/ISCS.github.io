0358ac3c5ed6eb71657a0233b6061a01e34c36e4
SSSIHLDMACS <sssihldmacs@gmail.com>
2015-12-08 19:23:32 +0530

    clean up

--- SC12tut161/OPT/dummy.c
+++
@@ -1,4 +0,0 @@
-
-void dummy(double *m) {}
-
-void dummy_(double *m) {}
--- fib.c
+++
@@ -1,8 +0,0 @@
-#include <inttypes.h>
-//#include <stdio.h>
-uint64_t fib(uint64_t n)
-{
-if (n < 2) { return n; }
-return (fib(n-1) + fib(n-2));
-}
-
